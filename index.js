f4de45e65db3cfd981e0f92b83137d4d32b52fae
simon <simon.emmott@yahoo.co.uk>
2022-08-06 18:37:20 +0100

    tested - working

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var pipe = require("./pipes/pipes.js").pipe;
 var cachingPipe = require("./pipes/pipes.js").cachingPipe;
-var queue = require("./queues/queues").queue;
+var queue = require("./queues/queues.js").queue;
+var captializeFirstLetter = require("./strings/strings").captializeFirstLetter;
+var titleCase = require("./strings/strings").titleCase;
+var snakeCase = require("./strings/strings").snakeCase;
+var kebabCase = require("./strings/strings").kebabCase;
+var camelCase = require("./strings/strings").camelCase;
+var aliasCase = require("./strings/strings").aliasCase;
 
 
 const utils = {
@@ -10,6 +16,14 @@ const utils = {
   },
   queues: {
     queue: queue
+  },
+  strings: {
+    captializeFirstLetter: captializeFirstLetter,
+    titleCase: titleCase,
+    snakeCase: snakeCase,
+    kebabCase: kebabCase,
+    camelCase: camelCase,
+    aliasCase: aliasCase
   }
 }
 
